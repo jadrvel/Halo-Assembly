4764ce51e6f894d9edc72f0bbc7c2d3793077a83
jadrvel <jadrvel@outlook.com>
2025-03-13 14:13:39 -0700

    Add `TagPatch`

--- src/Blamite/Patching/Patch.cs
+++ src/Blamite/Patching/Patch.cs
@@ -1,33 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
+#nullable enable
 namespace Blamite.Patching
 {
-	public class Patch
+	public class PatchInfo
 	{
-		public Patch()
-		{
-			MetaChanges = new List<DataChange>();
-			LanguageChanges = new List<LanguageChange>();
-			SegmentChanges = new List<SegmentChange>();
-			MapID = -1;
-			MetaChangesIndex = -1;
-			PC = false;
-		}
-
 		/// <summary>
 		///     The ID of the .map file that the patch is meant for. -1 if this information is not present.
 		/// </summary>
-		public int MapID { get; set; }
+		public int MapID { get; set; } = -1;
 
 		/// <summary>
 		///     The internal name of the .map file that the patch is meant for. Can be null.
 		/// </summary>
-		public string MapInternalName { get; set; }
+		public string? MapInternalName { get; set; }
 
 		/// <summary>
 		///     The desired name of the output .map file when the patch is applied. Can be null.
 		/// </summary>
-		public string OutputName { get; set; }
+		public string? OutputName { get; set; }
 
 		/// <summary>
 		///     The name of the patch.
@@ -47,19 +39,22 @@ namespace Blamite.Patching
 		/// <summary>
 		///     A screenshot of the patch in action.
 		/// </summary>
-		public byte[] Screenshot { get; set; }
+		public byte[]? Screenshot { get; set; }
 
 		/// <summary>
-		/// The build string of the original cache file.
+		///		The build string of the original cache file.
 		/// </summary>
-		public string BuildString { get; set; }
+		public string? BuildString { get; set; }
 
-		public bool PC { get; set; }
+		public bool PC { get; set; } = false;
+	}
 
+    public class Patch : PatchInfo
+	{
 		/// <summary>
 		///     Changes which should be made to different parts of a map file.
 		/// </summary>
-		public List<SegmentChange> SegmentChanges { get; private set; }
+		public List<SegmentChange> SegmentChanges { get; internal set; } = [];
 
 		/// <summary>
 		///     The base pointer to add to meta change offsets to get a pointer to poke to.
@@ -70,26 +65,36 @@ namespace Blamite.Patching
 		///     The index in <see cref="SegmentChanges" /> of the file's meta area changes.
 		///     Can be -1 if no meta change information is present in <see cref="SegmentChanges" />.
 		/// </summary>
-		public int MetaChangesIndex { get; set; }
+		public int MetaChangesIndex { get; set; } = -1;
 
 		/// <summary>
 		///     Embedded BLF and mapinfo files. Defaults to null.
 		/// </summary>
-		public BlfContent CustomBlfContent { get; set; }
+		public BlfContent? CustomBlfContent { get; set; }
 
 		#region Deprecated
 
 		/// <summary>
-		///     [DEPRECATED] Changes which should be made to the map file's meta area.
+		///     Changes which should be made to the map file's meta area.
 		///     The offset of each change is the meta pointer to where the change should be made.
 		/// </summary>
-		public List<DataChange> MetaChanges { get; private set; }
+		[Obsolete]
+		public List<DataChange> MetaChanges { get; internal set; } = [];
 
 		/// <summary>
-		///     [DEPRECATED] Changes that should be made to a map's locales.
+		///     Changes that should be made to a map's locales.
 		/// </summary>
-		public List<LanguageChange> LanguageChanges { get; private set; }
+		[Obsolete]
+		public List<LanguageChange> LanguageChanges { get; internal set; } = [];
 
-		#endregion Deprecated
+		#endregion
 	}
+
+	public class TagPatch : PatchInfo 
+	{
+		/// <summary>
+		///     Changes which should be made to different tags of map files.
+		/// </summary>
+		public List<TagChange> TagChanges { get; internal set; } = [];
+    }
 }
---
+++ src/Blamite/Patching/TagChange.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Blamite.Blam;
+
+namespace Blamite.Patching;
+
+[Flags]
+public enum BlockChangeFlag : byte
+{
+	None = 0,
+	TopLevel = 1 << 0,
+	DataRef = 1 << 1,
+	TagBlock = 1 << 2
+}
+
+public class BlockChange(uint baseSize) 
+{
+	public uint BaseSize { get; internal set; } = baseSize;
+
+	public List<DataChange> Changes { get; internal set; } = [];
+
+	public Dictionary<uint, List<DataChange>> DataRefChanges { get; } = new();
+
+    public Dictionary<uint, BlockChange> TagBlockChanges { get; } = new();
+
+	public BlockChangeFlag ChangeFlag 
+	{
+		get 
+		{
+			var flag = BlockChangeFlag.None;
+            if (Changes.Count > 0)
+				flag |= BlockChangeFlag.TopLevel;
+            if (DataRefChanges.Count > 0)
+                flag |= BlockChangeFlag.DataRef;
+            if (TagBlockChanges.Count > 0)
+                flag |= BlockChangeFlag.TagBlock;
+            return flag;
+        }
+	}
+}
+
+public class TagChange(string name, int group, uint baseSize) : BlockChange(baseSize)
+{
+	/// <summary>
+	///     The path of the tag in <see cref="ICacheFile"/>.
+	/// </summary>
+	public string Name { get; } = name;
+
+	/// <summary>
+	///    The magic of the group of the tag in <see cref="ITagGroup"/>.
+	/// </summary>
+	public int Group { get; } = group;
+}
